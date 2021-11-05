093f51c36cd09fe46c768de59352908f90ed2dfe
silver-it <silver.19920220@gmail.com>
2021-11-06 04:45:47 +0800

    Initialization

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <Text>Test</Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
